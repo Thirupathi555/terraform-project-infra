76eb350be58e0979d4e4246cac36ff9426d79e21
Thirupathi555 <thirupathisee339@gmail.com>
2024-10-09 23:25:11 +0530

    terraform

---
+++ 60-backend/main.tf
@@ -0,0 +1,32 @@
+module "backend" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  ami = data.aws_ami.joindevops.id
+  name = local.resource_name
+
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [local.backend_sg_id]
+  subnet_id              = local.private_subnet_id
+
+  tags = merge(
+    var.common_tags,
+    var.backend_tags,
+    {
+        Name = local.resource_name
+    }
+  )
+}
+
+resource "null_resource" "backend" {
+  # Changes to any instance of the cluster requires re-provisioning
+  triggers = {
+    instance_id = module.backend.id
+  }
+
+  # Bootstrap script can run on any instance of the cluster
+  # So we just choose the first in this case
+  connection {
+    host = module.backend.private_ip
+    type = "ssh"
+    user = "ec2-user"
+    password = "DevOps321"
+  }
