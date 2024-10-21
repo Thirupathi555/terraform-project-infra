c4a89b6ed64d89fb69776295b91155282d2d9e03
Thirupathi555 <thirupathisee339@gmail.com>
2024-10-21 11:55:05 +0530

    terraform

--- 10-vpc/vpc.tf
+++ 10-vpc/vpc.tf
@@ -1,7 +1,6 @@
 module "vpc" {
-    source = "../../terraform_aws_vpc"
-    #source = "git::https://github.com/Thirupathi555/terraform-aws-vpc.git?ref=main"
-    vpc_cidr = var.vpc_cidr
+    #source = "../../terraform_aws_vpc"
+    source = "git::https://github.com/Thirupathi555/terraform-aws-vpc.git?ref=main"
     project_name = var.project_name
     environment = var.environment
     common_tags = var.common_tags
